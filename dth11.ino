4334492f6637492c528f98975e7d1c665abd9559
ivan <ivan.azamar@tesch.edu.mx>
2025-11-06 11:46:44 -0600

    iot

---
+++ dth11.ino
@@ -0,0 +1,26 @@
+#include "DHT.h"
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (!isnan(h) && !isnan(t)) {
+    Serial.print("INSERT INTO lecturas (temperatura, humedad) VALUES (");
+    Serial.print(t);
+    Serial.print(",");
+    Serial.print(h);
+    Serial.println(");");
+  }
+
+  delay(5000); // cada 5 segundos
+}
